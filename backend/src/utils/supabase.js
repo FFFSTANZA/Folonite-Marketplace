8d279d494c797552b620f3608b14d5448c19241d
FFFSTANZA <147798939+FFFSTANZA@users.noreply.github.com>
2024-10-10 19:24:07 +0530

    Update supabase.js

--- backend/src/utils/supabase.js
+++ backend/src/utils/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Specify Supabase URL and Key directly here
-const supabaseUrl = '-----';
-const supabaseKey = '----';
+const supabaseUrl = 'SUPABASE_URL';
+const supabaseKey = 'SUPABASE_KEY';
 
 // Create the Supabase client using the hardcoded values
 export const supabase = createClient(supabaseUrl, supabaseKey);
